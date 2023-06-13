93514d9470fb39636cbcaef5770bbed164ef66fe
DamianT <elmo@DESKTOP-OP1RDAI>
2023-06-13 16:02:21 -0600

    err:borrar todo

--- js/postView.js
+++ js/postView.js
@@ -228,7 +228,7 @@ let userDataOnPost = (postToView) => {
 // almacena la imagen del creador del post
 let creatorProfilePicture = (postToView) => {
     let creatorPicture = document.createElement("div")
-        creatorPicture.classList.add("user-data-space")
+        creatorPicture.classList.add("user-data-space--post")
 
         let imgOnMini = document.createElement("div")
             imgOnMini.classList.add("user-data-space__mini")
