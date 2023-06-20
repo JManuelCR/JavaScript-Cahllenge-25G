e255d985b15660908e3fb5ae3e160a1781069b40
Jose Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2023-06-19 23:35:45 -0600

    endpoints called

--- js/DataBase.js
+++ js/DataBase.js
@@ -6,12 +6,71 @@ const getPosts = async () => {
     return posts;
   };
   
-const getUsers = async () =>{
-    let response= await fetch(
-        ``
+const deletePost = async (id) =>{
+  const token = localStorage.getItem('token');
+    let response = await fetch(
+        `http://localhost:8080/post/${id}`, {
+          method: DELETE,
+          headers: {
+            'Autorization': `Bearer ${token}`
+          }          
+        }
     )
 }
-export { getPosts };
+const editPost = async (id, data) =>{
+  const token = localStorage.getItem('token');
+    let response = await fetch(
+        `http://localhost:8080/post/${id}`, {
+          method: PATCH,
+          headers: {
+            'Autorization': `Bearer ${token}`
+          }, 
+          body: JSON.stringify(data)          
+        }
+    )
+}
+
+const getPostById = async (id) =>{
+  const token = localStorage.getItem('token');
+  let response = await fetch(
+    `http://localhost:8080/post/${id}`, {
+      method: GET,
+      headers: {
+        'Autorization': `Bearer ${token}`
+      }          
+    }
+)
+}
+const createUser = async (data) =>{
+    let response = await fetch(
+        `http://localhost:8080/`, {
+          method: POST,
+          body: JSON.stringify(data)
+        }
+    )
+}
+const login = async (data) => {
+  let reponse = await fetch(
+    `http://localhost:8080/auth`, {
+      method: POST,
+      body: JSON.stringify(data)
+    }
+  )
+}
+const addComment = async (id, data) => {
+  const token = localStorage.getItem('token');
+    let response = await fetch(
+        `http://localhost:8080/post/${id}`, {
+          method: PATCH,
+          headers: {
+            'Autorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(data)          
+        }
+    )
+}
+
+export { getPosts, deletePost, editPost, getPostById, createUser, login, addComment };
 // let getUsers = async ( ) => {
 //     let respomse = await fetch[
 //         ``
